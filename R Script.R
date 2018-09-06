b8f0b846ae188e9967bfc46de85436e3c5080812
Prashanthshe <41041692+Prashanthshe@users.noreply.github.com>
2018-09-06 12:13:05 +0530

    Add files via upload

---
+++ R Script.R
@@ -0,0 +1,11 @@
+df1 = data.frame(CustId = c(1:6), Product = c(rep("TV", 3), rep("Radio", 3)))
+df2 = data.frame(CustId = c(2, 4, 6), State = c(rep("Texas", 2), rep("NYC", 1)))
+df1 #left table
+df2 #right table
+1. Return only the rows in which the left table have match
+df<-merge(x=df1,y=df2,by="CustId")
+df
+
+2. Return all rows from both tables, join records from the left which have matching keys in the right table.
+df<-merge(x=df1,y=df2,by="CustId",all=TRUE)
+df
